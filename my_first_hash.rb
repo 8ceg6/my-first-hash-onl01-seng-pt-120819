7eeaae6873a7f62244335ec600d5ee99b963b769
cedric e goddard <8ceg6@comcast.net>
2019-12-19 04:58:20 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -32,7 +32,7 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-shipping_maifest[]
+shipping_maifest["muskets"]
 
 
   # add 4 gun powder to the shipping_manifest hash below
